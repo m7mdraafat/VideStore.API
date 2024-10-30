970476d37504ab24f65c340636e71561040edaa1
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-10-30 16:50:03 +0300

    - configure hangfire and data deletion job

---
+++ VideStore.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Hangfire;
+using HangfireBasicAuthenticationFilter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using VideStore.Domain.ConfigurationsData;
+using VideStore.Infrastructure.Utilities;
+
+namespace VideStore.Infrastructure;
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddHangfireConfigurations(this IServiceCollection services, string connectionString)
+    {
+        services.AddHangfire(x => x.UseSqlServerStorage(connectionString));
+
+        services.AddHangfireServer(); 
+
+
+        return services;
+    }
+
+    public static WebApplication UseHangfireDashboardAndRecurringJob(this WebApplication app, IServiceCollection services)
+    {
+        var serviceProvider = services.BuildServiceProvider();
+
+        var hangfireData = serviceProvider.GetRequiredService<IOptions<HangfireSettingsData>>().Value;
+
+        app.UseHangfireDashboard(hangfireData.DashboardUrl, new DashboardOptions()
+        {
+            Authorization = new[]
+            {
+                new HangfireCustomBasicAuthenticationFilter()
+                {
+                    User = hangfireData.UserName,
+                    Pass = hangfireData.Password
+                }
+            },
+            DashboardTitle = hangfireData.ServerName,
+        });
+
+        // Schedule recurring job
+        RecurringJob.AddOrUpdate<DataDeletionJob>("data-deletion-job", x => x.Execute(), Cron.Monthly(1));
+
+        return app;
+    }
+
+}
---
+++ VideStore.Infrastructure/Utilities/DataDeletionJob.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideStore.Presistence.Context;
+
+namespace VideStore.Infrastructure.Utilities
+{
+    public class DataDeletionJob(StoreDbContext storeDbContext)
+    {
+        public async Task Execute()
+        {
+            await storeDbContext.IdentityCodes
+                .Where(p => p.IsActive == false && p.CreationTime < DateTime.UtcNow.AddMinutes(-5))
+                .ForEachAsync(p => storeDbContext.IdentityCodes.Remove(p));
+
+            await storeDbContext.SaveChangesAsync();
+        }
+    }
+}
