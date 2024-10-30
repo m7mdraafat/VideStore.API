581310a23c2e05b5a3b5c1f1984a9f6f318b4252
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-10-30 16:48:31 +0300

    register services to dependency injection

---
+++ VideStore.Api/DependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using VideStore.Api.ServicesExtensions;
+using VideStore.Domain.ConfigurationsData;
+using VideStore.Infrastructure;
+using VideStore.Presistence;
+
+namespace VideStore.Api
+{
+    public static class DependencyInjection
+    {
+        public static WebApplicationBuilder AddApplicationDependencies(this WebApplicationBuilder builder)
+        {
+            var services = builder.Services;
+            var configuration = builder.Configuration;
+
+
+            services.ConfigureAppSettingsData(configuration);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var databaseConnections = serviceProvider.GetRequiredService<IOptions<DatabaseConnections>>().Value;
+            var jwtData = serviceProvider.GetRequiredService<IOptions<JwtData>>().Value;
+            var googleData = serviceProvider.GetRequiredService<IOptions<GoogleData>>().Value;
+            services.AddControllers();
+
+            services.AddSwaggerServices();
+
+            services.AddApiVersioningConfiguration(); 
+
+            services.AddStoreContext(databaseConnections.StoreConnection);
+
+            services.AddAuthConfigurations(jwtData, googleData);
+
+            services.AddIdentityConfigurations();
+
+            services.AddHangfireConfigurations(databaseConnections.HangfireConnection);
+
+            services.AddCorsPolicy(); 
+            return builder; 
+
+
+        }
+    }
+}
