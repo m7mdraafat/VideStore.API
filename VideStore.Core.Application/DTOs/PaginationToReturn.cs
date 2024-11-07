b7540c9568d73eeb3b67831dbcfd36740ed59aba
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-11-08 00:42:53 +0200

    add pagination class to specifiying the response of pagination

---
+++ VideStore.Core.Application/DTOs/PaginationToReturn.cs
@@ -0,0 +1,9 @@
+﻿namespace VideStore.Application.DTOs
+{
+    public record PaginationToReturn<T>(
+        int PageIndex,
+        int PageSize,
+        int Count,
+        IReadOnlyList<T> Data
+    );
+}
